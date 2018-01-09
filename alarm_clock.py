0f29392bf249d51b9f9afe27a80c03b741ddf5e6
drblur <ashwinkumar.chalkapure@gmail.com>
2018-01-09 13:25:44 +0530

    very first commit

---
+++ alarm_clock.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+
+## open the mp3 file that you want to play
+
+os.popen("xdg-open /home/ashwin/Downloads/cult_of_personality.mp3")
+
+
+
+
+## Exiting the program
+
+exit()
